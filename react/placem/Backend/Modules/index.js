cb1f31a76ddd2d2114978a1ea2db331db95bf300
pagal <pagalavanks.22cse@kongu.edu>
2025-01-06 15:25:13 +0530

    Day8

--- react/placem/Backend/Modules/index.js
+++ react/placem/Backend/Modules/index.js
@@ -6,7 +6,6 @@ const port = process.env.PORT
 var mdb = require('mongoose')
 var User=require('./user')
 
-
 let app = express();
 app.use(cors())
 app.use(express.json())
@@ -18,12 +17,9 @@ mdb.connect("mongodb://localhost:27017/placement")
     console.log(e);
 })
 
-
-
 app.get('/',(req,res)=>{
     res.send('Hello World!')
 })
-
 
 app.get('/static',(req,res)=>{
     res.sendFile(path.join(__dirname,'index.html'))
@@ -58,7 +54,6 @@ app.get('/users',async(req,res)=>{
     var log =await User.find()
     res.send(log);
 })
-
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${process.env.PORT}`);
